6225e640dc17c177dbf574ad821121eb28186130
Joe Geldart <joe@joegeldart.com>
2010-12-17 22:39:51 +0000

    Add a visualisation of renewables

---
+++ data/eu-borders.js
@@ -0,0 +1,1130 @@
+var eu_borders = [
+    {
+      "code" : "AT",
+      "code2" : "AT"
+    },
+    {
+      "code" : "AT",
+      "code2" : "IT"
+    },
+    {
+      "code" : "AT",
+      "code2" : "AT"
+    },
+    {
+      "code" : "AT",
+      "code2" : "AT"
+    },
+    {
+      "code" : "AT",
+      "code2" : "DE"
+    },
+    {
+      "code" : "AT",
+      "code2" : "AT"
+    },
+    {
+      "code" : "AT",
+      "code2" : "SK"
+    },
+    {
+      "code" : "AT",
+      "code2" : "AT"
+    },
+    {
+      "code" : "AT",
+      "code2" : "CH"
+    },
+    {
+      "code" : "AT",
+      "code2" : "AT"
+    },
+    {
+      "code" : "AT",
+      "code2" : "HU"
+    },
+    {
+      "code" : "AT",
+      "code2" : "AT"
+    },
+    {
+      "code" : "AT",
+      "code2" : "LI"
+    },
+    {
+      "code" : "AT",
+      "code2" : "AT"
+    },
+    {
+      "code" : "AT",
+      "code2" : "SI"
+    },
+    {
+      "code" : "BE",
+      "code2" : "FR"
+    },
+    {
+      "code" : "BE",
+      "code2" : "BE"
+    },
+    {
+      "code" : "BE",
+      "code2" : "LU"
+    },
+    {
+      "code" : "BE",
+      "code2" : "BE"
+    },
+    {
+      "code" : "BE",
+      "code2" : "DE"
+    },
+    {
+      "code" : "BE",
+      "code2" : "BE"
+    },
+    {
+      "code" : "BE",
+      "code2" : "BE"
+    },
+    {
+      "code" : "BE",
+      "code2" : "NL"
+    },
+    {
+      "code" : "BE",
+      "code2" : "BE"
+    },
+    {
+      "code" : "BE",
+      "code2" : "BE"
+    },
+    {
+      "code" : "BE",
+      "code2" : "BE"
+    },
+    {
+      "code" : "BG",
+      "code2" : "BG"
+    },
+    {
+      "code" : "BG",
+      "code2" : "RO"
+    },
+    {
+      "code" : "BG",
+      "code2" : "TR"
+    },
+    {
+      "code" : "BG",
+      "code2" : "BG"
+    },
+    {
+      "code" : "BG",
+      "code2" : "GR"
+    },
+    {
+      "code" : "BG",
+      "code2" : "BG"
+    },
+    {
+      "code" : "BG",
+      "code2" : "CS"
+    },
+    {
+      "code" : "BG",
+      "code2" : "BG"
+    },
+    {
+      "code" : "BG",
+      "code2" : "MK"
+    },
+    {
+      "code" : "BG",
+      "code2" : "BG"
+    },
+    {
+      "code" : "BG",
+      "code2" : "BG"
+    },
+    {
+      "code" : "CH",
+      "code2" : "CH"
+    },
+    {
+      "code" : "CH",
+      "code2" : "CH"
+    },
+    {
+      "code" : "CH",
+      "code2" : "IT"
+    },
+    {
+      "code" : "CH",
+      "code2" : "CH"
+    },
+    {
+      "code" : "CH",
+      "code2" : "CH"
+    },
+    {
+      "code" : "CH",
+      "code2" : "DE"
+    },
+    {
+      "code" : "CH",
+      "code2" : "FR"
+    },
+    {
+      "code" : "CH",
+      "code2" : "CH"
+    },
+    {
+      "code" : "CH",
+      "code2" : "AT"
+    },
+    {
+      "code" : "CH",
+      "code2" : "CH"
+    },
+    {
+      "code" : "CH",
+      "code2" : "LI"
+    },
+    {
+      "code" : "CH",
+      "code2" : "CH"
+    },
+    {
+      "code" : "CY",
+      "code2" : null
+    },
+    {
+      "code" : "CZ",
+      "code2" : "CZ"
+    },
+    {
+      "code" : "CZ",
+      "code2" : "PL"
+    },
+    {
+      "code" : "CZ",
+      "code2" : "CZ"
+    },
+    {
+      "code" : "CZ",
+      "code2" : "DE"
+    },
+    {
+      "code" : "CZ",
+      "code2" : "CZ"
+    },
+    {
+      "code" : "CZ",
+      "code2" : null
+    },
+    {
+      "code" : "CZ",
+      "code2" : "CZ"
+    },
+    {
+      "code" : "CZ",
+      "code2" : "SK"
+    },
+    {
+      "code" : "DE",
+      "code2" : "DE"
+    },
+    {
+      "code" : "DE",
+      "code2" : "PL"
+    },
+    {
+      "code" : "DE",
+      "code2" : "DE"
+    },
+    {
+      "code" : "DE",
+      "code2" : "BE"
+    },
+    {
+      "code" : "DE",
+      "code2" : "DE"
+    },
+    {
+      "code" : "DE",
+      "code2" : "NL"
+    },
+    {
+      "code" : "DE",
+      "code2" : "AT"
+    },
+    {
+      "code" : "DE",
+      "code2" : "DE"
+    },
+    {
+      "code" : "DE",
+      "code2" : "CH"
+    },
+    {
+      "code" : "DE",
+      "code2" : "DE"
+    },
+    {
+      "code" : "DE",
+      "code2" : "FR"
+    },
+    {
+      "code" : "DE",
+      "code2" : "DE"
+    },
+    {
+      "code" : "DE",
+      "code2" : "DK"
+    },
+    {
+      "code" : "DE",
+      "code2" : "DE"
+    },
+    {
+      "code" : "DE",
+      "code2" : "CZ"
+    },
+    {
+      "code" : "DE",
+      "code2" : "DE"
+    },
+    {
+      "code" : "DE",
+      "code2" : "LU"
+    },
+    {
+      "code" : "DE",
+      "code2" : "DE"
+    },
+    {
+      "code" : "DK",
+      "code2" : "DK"
+    },
+    {
+      "code" : "DK",
+      "code2" : "DE"
+    },
+    {
+      "code" : "EE",
+      "code2" : "LV"
+    },
+    {
+      "code" : "EE",
+      "code2" : "EE"
+    },
+    {
+      "code" : "EE",
+      "code2" : "RU"
+    },
+    {
+      "code" : "EE",
+      "code2" : "EE"
+    },
+    {
+      "code" : "ES",
+      "code2" : "ES"
+    },
+    {
+      "code" : "ES",
+      "code2" : "FR"
+    },
+    {
+      "code" : "ES",
+      "code2" : "ES"
+    },
+    {
+      "code" : "ES",
+      "code2" : "MA"
+    },
+    {
+      "code" : "ES",
+      "code2" : "PT"
+    },
+    {
+      "code" : "ES",
+      "code2" : "ES"
+    },
+    {
+      "code" : "ES",
+      "code2" : "ES"
+    },
+    {
+      "code" : "ES",
+      "code2" : "AD"
+    },
+    {
+      "code" : "ES",
+      "code2" : "GI"
+    },
+    {
+      "code" : "ES",
+      "code2" : "ES"
+    },
+    {
+      "code" : "FI",
+      "code2" : "FI"
+    },
+    {
+      "code" : "FI",
+      "code2" : "SE"
+    },
+    {
+      "code" : "FI",
+      "code2" : "FI"
+    },
+    {
+      "code" : "FI",
+      "code2" : "NO"
+    },
+    {
+      "code" : "FI",
+      "code2" : "FI"
+    },
+    {
+      "code" : "FI",
+      "code2" : "FI"
+    },
+    {
+      "code" : "FI",
+      "code2" : "RU"
+    },
+    {
+      "code" : "FR",
+      "code2" : "FR"
+    },
+    {
+      "code" : "FR",
+      "code2" : "IT"
+    },
+    {
+      "code" : "FR",
+      "code2" : "FR"
+    },
+    {
+      "code" : "FR",
+      "code2" : "BE"
+    },
+    {
+      "code" : "FR",
+      "code2" : "FR"
+    },
+    {
+      "code" : "FR",
+      "code2" : "FR"
+    },
+    {
+      "code" : "FR",
+      "code2" : "DE"
+    },
+    {
+      "code" : "FR",
+      "code2" : "ES"
+    },
+    {
+      "code" : "FR",
+      "code2" : "FR"
+    },
+    {
+      "code" : "FR",
+      "code2" : "FR"
+    },
+    {
+      "code" : "FR",
+      "code2" : "FR"
+    },
+    {
+      "code" : "FR",
+      "code2" : "CH"
+    },
+    {
+      "code" : "FR",
+      "code2" : "AD"
+    },
+    {
+      "code" : "FR",
+      "code2" : "FR"
+    },
+    {
+      "code" : "FR",
+      "code2" : "LU"
+    },
+    {
+      "code" : "FR",
+      "code2" : "FR"
+    },
+    {
+      "code" : "FR",
+      "code2" : "MC"
+    },
+    {
+      "code" : "FR",
+      "code2" : "FR"
+    },
+    {
+      "code" : "GR",
+      "code2" : "GR"
+    },
+    {
+      "code" : "GR",
+      "code2" : "BG"
+    },
+    {
+      "code" : "GR",
+      "code2" : "GR"
+    },
+    {
+      "code" : "GR",
+      "code2" : "TR"
+    },
+    {
+      "code" : "GR",
+      "code2" : "GR"
+    },
+    {
+      "code" : "GR",
+      "code2" : "AL"
+    },
+    {
+      "code" : "GR",
+      "code2" : "GR"
+    },
+    {
+      "code" : "GR",
+      "code2" : "MK"
+    },
+    {
+      "code" : "GR",
+      "code2" : "GR"
+    },
+    {
+      "code" : "HR",
+      "code2" : "HR"
+    },
+    {
+      "code" : "HR",
+      "code2" : "HU"
+    },
+    {
+      "code" : "HR",
+      "code2" : "ME"
+    },
+    {
+      "code" : "HR",
+      "code2" : "HR"
+    },
+    {
+      "code" : "HR",
+      "code2" : "HR"
+    },
+    {
+      "code" : "HR",
+      "code2" : "SI"
+    },
+    {
+      "code" : "HR",
+      "code2" : "HR"
+    },
+    {
+      "code" : "HR",
+      "code2" : "CS"
+    },
+    {
+      "code" : "HR",
+      "code2" : "HR"
+    },
+    {
+      "code" : "HR",
+      "code2" : "BA"
+    },
+    {
+      "code" : "HU",
+      "code2" : "HU"
+    },
+    {
+      "code" : "HU",
+      "code2" : "UA"
+    },
+    {
+      "code" : "HU",
+      "code2" : "HU"
+    },
+    {
+      "code" : "HU",
+      "code2" : "RO"
+    },
+    {
+      "code" : "HU",
+      "code2" : "HU"
+    },
+    {
+      "code" : "HU",
+      "code2" : "SK"
+    },
+    {
+      "code" : "HU",
+      "code2" : "CS"
+    },
+    {
+      "code" : "HU",
+      "code2" : "HU"
+    },
+    {
+      "code" : "HU",
+      "code2" : "AT"
+    },
+    {
+      "code" : "HU",
+      "code2" : "HU"
+    },
+    {
+      "code" : "HU",
+      "code2" : "HR"
+    },
+    {
+      "code" : "HU",
+      "code2" : "HU"
+    },
+    {
+      "code" : "HU",
+      "code2" : "SI"
+    },
+    {
+      "code" : "HU",
+      "code2" : "HU"
+    },
+    {
+      "code" : "IE",
+      "code2" : "IE"
+    },
+    {
+      "code" : "IE",
+      "code2" : "GB"
+    },
+    {
+      "code" : "IE",
+      "code2" : "IE"
+    },
+    {
+      "code" : "IE",
+      "code2" : null
+    },
+    {
+      "code" : "IE",
+      "code2" : "IE"
+    },
+    {
+      "code" : "IS",
+      "code2" : null
+    },
+    {
+      "code" : "IT",
+      "code2" : "FR"
+    },
+    {
+      "code" : "IT",
+      "code2" : "IT"
+    },
+    {
+      "code" : "IT",
+      "code2" : "CH"
+    },
+    {
+      "code" : "IT",
+      "code2" : "IT"
+    },
+    {
+      "code" : "IT",
+      "code2" : "AT"
+    },
+    {
+      "code" : "IT",
+      "code2" : "IT"
+    },
+    {
+      "code" : "IT",
+      "code2" : "SI"
+    },
+    {
+      "code" : "IT",
+      "code2" : "IT"
+    },
+    {
+      "code" : "IT",
+      "code2" : "IT"
+    },
+    {
+      "code" : "LT",
+      "code2" : "LT"
+    },
+    {
+      "code" : "LT",
+      "code2" : "PL"
+    },
+    {
+      "code" : "LT",
+      "code2" : "LT"
+    },
+    {
+      "code" : "LT",
+      "code2" : "LT"
+    },
+    {
+      "code" : "LT",
+      "code2" : null
+    },
+    {
+      "code" : "LT",
+      "code2" : "LT"
+    },
+    {
+      "code" : "LT",
+      "code2" : "LT"
+    },
+    {
+      "code" : "LT",
+      "code2" : "LT"
+    },
+    {
+      "code" : "LT",
+      "code2" : "LT"
+    },
+    {
+      "code" : "LT",
+      "code2" : "RU"
+    },
+    {
+      "code" : "LT",
+      "code2" : "LT"
+    },
+    {
+      "code" : "LT",
+      "code2" : "LV"
+    },
+    {
+      "code" : "LT",
+      "code2" : "LT"
+    },
+    {
+      "code" : "LT",
+      "code2" : "BY"
+    },
+    {
+      "code" : "LU",
+      "code2" : "LU"
+    },
+    {
+      "code" : "LU",
+      "code2" : "BE"
+    },
+    {
+      "code" : "LU",
+      "code2" : "LU"
+    },
+    {
+      "code" : "LU",
+      "code2" : "DE"
+    },
+    {
+      "code" : "LU",
+      "code2" : "LU"
+    },
+    {
+      "code" : "LU",
+      "code2" : "FR"
+    },
+    {
+      "code" : "LV",
+      "code2" : "LV"
+    },
+    {
+      "code" : "LV",
+      "code2" : null
+    },
+    {
+      "code" : "LV",
+      "code2" : "LV"
+    },
+    {
+      "code" : "LV",
+      "code2" : "LT"
+    },
+    {
+      "code" : "LV",
+      "code2" : "LV"
+    },
+    {
+      "code" : "LV",
+      "code2" : "LV"
+    },
+    {
+      "code" : "LV",
+      "code2" : "EE"
+    },
+    {
+      "code" : "LV",
+      "code2" : "LV"
+    },
+    {
+      "code" : "LV",
+      "code2" : "BY"
+    },
+    {
+      "code" : "LV",
+      "code2" : "LV"
+    },
+    {
+      "code" : "LV",
+      "code2" : "RU"
+    },
+    {
+      "code" : "NL",
+      "code2" : "DE"
+    },
+    {
+      "code" : "NL",
+      "code2" : "NL"
+    },
+    {
+      "code" : "NL",
+      "code2" : "BE"
+    },
+    {
+      "code" : "NL",
+      "code2" : "NL"
+    },
+    {
+      "code" : "NL",
+      "code2" : "NL"
+    },
+    {
+      "code" : "NO",
+      "code2" : "NO"
+    },
+    {
+      "code" : "NO",
+      "code2" : "RU"
+    },
+    {
+      "code" : "NO",
+      "code2" : "NO"
+    },
+    {
+      "code" : "NO",
+      "code2" : "SE"
+    },
+    {
+      "code" : "NO",
+      "code2" : "NO"
+    },
+    {
+      "code" : "NO",
+      "code2" : "FI"
+    },
+    {
+      "code" : "PL",
+      "code2" : "DE"
+    },
+    {
+      "code" : "PL",
+      "code2" : "PL"
+    },
+    {
+      "code" : "PL",
+      "code2" : "CZ"
+    },
+    {
+      "code" : "PL",
+      "code2" : "PL"
+    },
+    {
+      "code" : "PL",
+      "code2" : "SK"
+    },
+    {
+      "code" : "PL",
+      "code2" : "PL"
+    },
+    {
+      "code" : "PL",
+      "code2" : "PL"
+    },
+    {
+      "code" : "PL",
+      "code2" : "UA"
+    },
+    {
+      "code" : "PL",
+      "code2" : "BY"
+    },
+    {
+      "code" : "PL",
+      "code2" : "PL"
+    },
+    {
+      "code" : "PL",
+      "code2" : "RU"
+    },
+    {
+      "code" : "PL",
+      "code2" : "PL"
+    },
+    {
+      "code" : "PL",
+      "code2" : "LT"
+    },
+    {
+      "code" : "PL",
+      "code2" : "PL"
+    },
+    {
+      "code" : "PL",
+      "code2" : "PL"
+    },
+    {
+      "code" : "PL",
+      "code2" : null
+    },
+    {
+      "code" : "PL",
+      "code2" : "PL"
+    },
+    {
+      "code" : "PT",
+      "code2" : "PT"
+    },
+    {
+      "code" : "PT",
+      "code2" : "ES"
+    },
+    {
+      "code" : "PT",
+      "code2" : "PT"
+    },
+    {
+      "code" : "RO",
+      "code2" : "RO"
+    },
+    {
+      "code" : "RO",
+      "code2" : "UA"
+    },
+    {
+      "code" : "RO",
+      "code2" : "RO"
+    },
+    {
+      "code" : "RO",
+      "code2" : "MD"
+    },
+    {
+      "code" : "RO",
+      "code2" : "HU"
+    },
+    {
+      "code" : "RO",
+      "code2" : "RO"
+    },
+    {
+      "code" : "RO",
+      "code2" : "BG"
+    },
+    {
+      "code" : "RO",
+      "code2" : "RO"
+    },
+    {
+      "code" : "RO",
+      "code2" : "CS"
+    },
+    {
+      "code" : "RO",
+      "code2" : "RO"
+    },
+    {
+      "code" : "RO",
+      "code2" : "RO"
+    },
+    {
+      "code" : "SE",
+      "code2" : "NO"
+    },
+    {
+      "code" : "SE",
+      "code2" : "SE"
+    },
+    {
+      "code" : "SE",
+      "code2" : "FI"
+    },
+    {
+      "code" : "SE",
+      "code2" : "SE"
+    },
+    {
+      "code" : "SI",
+      "code2" : "SI"
+    },
+    {
+      "code" : "SI",
+      "code2" : "IT"
+    },
+    {
+      "code" : "SI",
+      "code2" : "SI"
+    },
+    {
+      "code" : "SI",
+      "code2" : "HU"
+    },
+    {
+      "code" : "SI",
+      "code2" : "HR"
+    },
+    {
+      "code" : "SI",
+      "code2" : "SI"
+    },
+    {
+      "code" : "SI",
+      "code2" : "AT"
+    },
+    {
+      "code" : "SI",
+      "code2" : "SI"
+    },
+    {
+      "code" : "SK",
+      "code2" : "SK"
+    },
+    {
+      "code" : "SK",
+      "code2" : "PL"
+    },
+    {
+      "code" : "SK",
+      "code2" : "SK"
+    },
+    {
+      "code" : "SK",
+      "code2" : "UA"
+    },
+    {
+      "code" : "SK",
+      "code2" : "CZ"
+    },
+    {
+      "code" : "SK",
+      "code2" : "SK"
+    },
+    {
+      "code" : "SK",
+      "code2" : "HU"
+    },
+    {
+      "code" : "SK",
+      "code2" : "SK"
+    },
+    {
+      "code" : "SK",
+      "code2" : "AT"
+    },
+    {
+      "code" : "SK",
+      "code2" : "SK"
+    },
+    {
+      "code" : "TR",
+      "code2" : "TR"
+    },
+    {
+      "code" : "TR",
+      "code2" : "BG"
+    },
+    {
+      "code" : "TR",
+      "code2" : "IR"
+    },
+    {
+      "code" : "TR",
+      "code2" : "TR"
+    },
+    {
+      "code" : "TR",
+      "code2" : "GE"
+    },
+    {
+      "code" : "TR",
+      "code2" : "TR"
+    },
+    {
+      "code" : "TR",
+      "code2" : "SY"
+    },
+    {
+      "code" : "TR",
+      "code2" : "TR"
+    },
+    {
+      "code" : "TR",
+      "code2" : "IQ"
+    },
+    {
+      "code" : "TR",
+      "code2" : "TR"
+    },
+    {
+      "code" : "TR",
+      "code2" : "AM"
+    },
+    {
+      "code" : "TR",
+      "code2" : "TR"
+    },
+    {
+      "code" : "TR",
+      "code2" : "TR"
+    },
+    {
+      "code" : "TR",
+      "code2" : "TR"
+    },
+    {
+      "code" : "TR",
+      "code2" : "GR"
+    },
+    {
+      "code" : "TR",
+      "code2" : "TR"
+    },
+    {
+      "code" : "TR",
+      "code2" : "AZ"
+    },
+    {
+      "code" : "TR",
+      "code2" : "TR"
+    },
+    {
+      "code" : "UK",
+      "code2" : "IE"
+    },
+    {
+      "code" : "UK",
+      "code2" : "GB"
+    },
+    {
+      "code" : "UK",
+      "code2" : "GB"
+    },
+    {
+      "code" : "UK",
+      "code2" : "FR"
+    }
+  ];
